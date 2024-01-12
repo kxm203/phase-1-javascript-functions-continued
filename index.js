7c16e2ea31cf6bf6dcafcf304d673e719cbede1a
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-12 08:36:59 -0500

    Pretty cool!

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    //console.log(`"This Saturday, I want to ${activity}!"`);
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (work = "go to the office") {
+    //console.log(`"This Monday I will ${work}."`);
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(symbol) {
+    const part1 = "You are";
+    return function (emphatic) {
+        console.log(`${part1} ${symbol} ${emphatic} ${symbol}!`);
+        return `${part1} ${symbol}${emphatic}${symbol}!`
+    }
+}
+wrapAdjective("%")(" a dedicated programmer ");
